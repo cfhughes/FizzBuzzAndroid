ee655a3afa84f2191353fe0738b3ad387afbf595
Chris Hughes <chris@speedycomputing.com>
2018-01-26 09:32:45 -0700

    Error handling for number parsing

--- app/src/main/java/edu/cnm/deepdive/fizzbuzzandroid/FizzBuzzActivity.java
+++ app/src/main/java/edu/cnm/deepdive/fizzbuzzandroid/FizzBuzzActivity.java
@@ -6,23 +6,36 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class FizzBuzzActivity extends AppCompatActivity {
+
+  TextView text;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_fizz_buzz);
-    final TextView text = (TextView) findViewById(R.id.text_view);
+    ScrollView scroll_view = (ScrollView) findViewById(R.id.scroll_view);
+
+    text = new TextView(this);
     text.setText(FizzBuzz.count(20));
+
+    scroll_view.addView(text);
 
     final EditText limit = (EditText) findViewById(R.id.limit);
     Button button = (Button) findViewById(R.id.button);
     button.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
-        text.setText(FizzBuzz.count(Integer.parseInt(limit.getText().toString())));
+        int limitInt = 0;
+        try {
+          limitInt = Integer.parseInt(limit.getText().toString());
+        } catch (NumberFormatException e) {
+          e.printStackTrace();
+        }
+        text.setText(FizzBuzz.count(limitInt));
       }
     });
   }
